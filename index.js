3d0f4fac0be1ee3c18af92fb6ea7139b8c6348ae
Jaghit <iamjaghitsingh@gmail.com>
2017-04-05 16:01:46 +0530

    common index.js for both (android & ios)

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    AppRegistry,
+    Text,
+} from 'react-native';
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <Text>hello world</Text>
+        );
+    }
+}
