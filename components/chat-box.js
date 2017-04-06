30c38c09c57094b88972917a7449b4ff02d78130
akshay1293 <akshayaggarwal1293@gmail.com>
2017-04-06 14:28:38 +0530

    creating chat box component

---
+++ components/chat-box.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button
+} from 'react-native';
+
+export default class ChatBox extends Component{
+
+    render(){
+
+        return(
+
+            
+        );
+    }
+}
