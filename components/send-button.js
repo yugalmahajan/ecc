5cb8aa181205ecadf34521fba1fc8725ac3036f7
Jaghit <iamjaghitsingh@gmail.com>
2017-04-07 12:42:38 +0530

    send button changed

--- components/send-button.js
+++ components/send-button.js
@@ -13,7 +13,7 @@ export default class SendButton extends Component {
 
         return (
 
-            <Image source={require('../assets/icons/send-1-icon.png')} style={styles.container}></Image>
+            <Image source={require('../assets/icons/send-2-icon.png')} style={styles.container}></Image>
         );
     }
 }
