fda0a10e349f508c98ffad8005bcec612b580d6d
Jaghit <iamjaghitsingh@gmail.com>
2017-04-07 17:56:22 +0530

    Default chat message 'hello' for testing purpose.

--- components/home.js
+++ components/home.js
@@ -22,7 +22,7 @@ export default class Home extends Component {
         this.state = {
 
             message: '',
-            messageStore: [],
+            messageStore: ['hello'],
         }
 
     }
